300fc56c4907ce39e5a68108df69fb46d5f9fcc3
Matthew C Schmidt <mattcschmidt@hotmail.com>
2024-10-24 14:41:23 +0800

    Added ObjectiveModel and ObjectiveViewModel

---
+++ ObjectiveModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ObjectivesAP
+{
+    public class ObjectiveModel
+    {
+        public string StructureId { get; internal set; }
+        public double Priority { get; internal set; }
+        public string Operation { get; internal set; }
+        public string Dose { get; internal set; }
+        public string Volume { get; internal set; }
+    }
+}
---
+++ ObjectiveViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMS.TPS.Common.Model.API;
+
+namespace ObjectivesAP
+{
+    public class ObjectiveViewModel
+    {
+        //Constructor (CTOR + TAB + TAB)
+        public List<ObjectiveModel> Objectives { get; set; }//make ObjectiveModel public.
+        public ObjectiveViewModel(PlanSetup plan)
+        {
+            Objectives = new List<ObjectiveModel>();
+            foreach(var objective in plan.OptimizationSetup.Objectives)
+            {
+                ObjectiveModel obj = new ObjectiveModel();
+                obj.StructureId = objective.StructureId;
+                obj.Priority = objective.Priority;
+                obj.Operation = objective.Operator.ToString();
+                if(objective is OptimizationPointObjective)
+                {
+                    obj.Dose = (objective as OptimizationPointObjective).Dose.ToString();
+                    obj.Volume = (objective as OptimizationPointObjective).Volume.ToString("F1");
+                }
+                else if(objective is OptimizationMeanDoseObjective)
+                {
+                    obj.Dose = (objective as OptimizationMeanDoseObjective).Dose.ToString();
+                    obj.Volume = "-";// (objective as OptimizationPointObjective).Volume.ToString("F1");
+                }
+                Objectives.Add(obj);
+            }
+        }
+    }
+}
