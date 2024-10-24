d55f6609bf417cfd7574e1a3eaf18f7d651d2e81
Matthew C Schmidt <mattcschmidt@hotmail.com>
2024-10-24 14:08:33 +0800

    Add project files.

---
+++ ObjectivesAP.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+using VMS.TPS.VisualScripting.ElementInterface;
+using System.Windows.Documents;
+using System.Windows.Controls;
+
+// TODO: Replace the following version attributes by creating AssemblyInfo.cs. You can do this in the properties of the Visual Studio project.
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyInformationalVersion("1.0")]
+
+namespace ObjectivesAP
+{
+    // TODO: Replace the existing class name with your own class name. (CTRL + R + R)
+    public class ObjectivesPackElement : VisualScriptElement
+    {
+        public ObjectivesPackElement()
+        {
+            this.CategorizationTag = "Custom Action Packs";
+            this.Description = new ElementDescription
+            {
+                ElementFunction = "Tabulate the optimization objectives",
+                Inputs = "Plan Setup",
+                Output = "To Flow Action Pack"
+            };
+        }
+        public ObjectivesPackElement(IVisualScriptElementRuntimeHost host) { }
+
+        public override bool RequiresRuntimeConsole { get { return false; } }
+        public override bool RequiresDatabaseModifications { get { return false; } }
+        //For custom user Interface Element set RequiresDetailedView => true;
+        public override bool RequiresDetailedView => true;//STEP 1
+
+
+        [ActionPackExecuteMethod]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public IEnumerable<BlockUIContainer> Execute(PlanSetup ps)
+        {
+            // TODO: Add your code here. Save this until the end.
+            List<BlockUIContainer> blocks = new List<BlockUIContainer>();
+            //how to get the data from the XML file
+            ObjectivesViewModel.ObjectivesModel objSelection =
+                (ObjectivesViewModel.ObjectivesModel)(m_designTimeDetails as//cast Data as ObjModel
+                ObjectivesViewModel.ObjectivesDesignTimeDetails).Data;//Getting the Data.
+            blocks.Add(
+                new BlockUIContainer(
+                new TextBlock//for TextBlock you will need using System.Windows.Controls. (lightbulb).
+                {
+                    Text = $"Objectives: {objSelection.ObjectivesChecked}" +
+                    $"\nParameters:{objSelection.ParametersChecked}"
+                }
+                )
+                );
+
+            return blocks;
+        }
+
+        public override string DisplayName
+        {
+            get
+            {
+                // TODO: Replace "Element Name" with the name that you want to be displayed in the Visual Scripting UI.
+                return "Optimization Objectives";
+            }
+        }
+        //STEP 2
+        //Must comment out the Allowed Options to have custom UI. (CTRL + K + C)
+        //IDictionary<string, string> m_options = new Dictionary<string, string>();
+        //public override void SetOption(string key, string value)
+        //{
+        //    m_options.Add(key, value);
+        //}
+
+        //public override IEnumerable<KeyValuePair<string, IEnumerable<string>>> AllowedOptions
+        //{
+        //    get
+        //    {
+        //        return new KeyValuePair<string, IEnumerable<string>>[] {
+        //    new KeyValuePair<string, IEnumerable<string>>("TestOption", new string[] { "Test Value" })
+        //  };
+        //    }
+        //}
+        //STEP 3 Provide the Design Time Details
+        private IDesignTimeDetails m_designTimeDetails { get; set; }
+        public override IDesignTimeDetails DesignTimeDetails
+        {
+            get
+            {
+                if (m_designTimeDetails == null)
+                {
+                    //TODO Add The UI to be opened when DesignTime Details is invoked
+                    m_designTimeDetails = new ObjectivesViewModel.ObjectivesDesignTimeDetails();
+                }
+                return m_designTimeDetails;
+            }
+        }
+    }
+}
---
+++ ObjectivesViewModel.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using VMS.TPS.Common.Model.Types;
+using VMS.TPS.VisualScripting.ElementInterface;
+
+namespace ObjectivesAP
+{
+    public class ObjectivesViewModel
+    {
+        public class ObjectivesModel
+        {
+            //create a property (prop+TAB+TAB)
+            public bool ObjectivesChecked { get; set; }
+            public bool ParametersChecked { get; set; }
+        }
+        // The IDesignTimeDetails is the interface to Visual Scripting. 
+        public class ObjectivesDesignTimeDetails : IDesignTimeDetails
+        {
+            //This property is the viewmodel to my view.
+            public ObjectivesModel LocalObjectivesModel { get; set; }
+            //This property is the default for my viewmodel in case the user cancels changes.
+            public ObjectivesModel KeepObjectivesModel { get; set; }
+            //UI that opens when the user clicks the "i"
+            public UserControl View
+            {
+                get
+                {
+                    //New action pack, need default selections.
+                    if(LocalObjectivesModel == null)
+                    {
+                        LocalObjectivesModel = new ObjectivesModel();
+                        LocalObjectivesModel.ObjectivesChecked = true;//default.
+                        KeepObjectivesModel = new ObjectivesModel();//instantiate class when UI is open.
+                        KeepObjectivesModel.ObjectivesChecked = true;//default.
+                    }
+                    return new ObjectivesDesignTimeView { DataContext = LocalObjectivesModel };
+                }
+            }
+            //Object going to and from the XML file.
+            public object Data 
+            {
+                //get => save data to the xml file.
+                get
+                {
+                    //if the UI hasn't been opened, set some defaults.
+                    if (LocalObjectivesModel == null)
+                    {
+                        LocalObjectivesModel = new ObjectivesModel();
+                        LocalObjectivesModel.ObjectivesChecked = true;//default.
+                    }
+                    return LocalObjectivesModel;
+                }
+                //set => pull data from the XML file.
+                set
+                {
+                    ObjectivesModel oModel = value as ObjectivesModel;
+                    //if oModel exists, set defaults on oModel, else set normal defaults.
+                    if(oModel != null)
+                    {
+                        LocalObjectivesModel = new ObjectivesModel();
+                        LocalObjectivesModel.ObjectivesChecked = oModel.ObjectivesChecked;
+                        LocalObjectivesModel.ParametersChecked = oModel.ParametersChecked;
+                        //assign also the keep viewmodel
+                        KeepObjectivesModel = new ObjectivesModel();
+                        KeepObjectivesModel.ObjectivesChecked = oModel.ObjectivesChecked;
+                        KeepObjectivesModel.ParametersChecked = oModel.ParametersChecked;
+                    }
+                    else//no XML file to read from, select the defaults. 
+                    {
+                        LocalObjectivesModel = new ObjectivesModel();
+                        LocalObjectivesModel.ObjectivesChecked = true;
+                        KeepObjectivesModel = new ObjectivesModel();
+                        KeepObjectivesModel.ObjectivesChecked = true;
+                    }
+                }
+            }
+            //XML to know what type of object to save.
+            public Type DataType => typeof(ObjectivesModel);
+
+            public Type[] IncomingType { get; set; }
+            public DoseValue.DoseUnit EclipseDoseUnit { get; set; }
+            //allows to check if user put data in correctly. (set to true for now).
+            public bool IsContentValid => true;
+
+            public void DiscardChanges()
+            {
+                LocalObjectivesModel.ObjectivesChecked = KeepObjectivesModel.ObjectivesChecked;
+                LocalObjectivesModel.ParametersChecked = KeepObjectivesModel.ParametersChecked;
+            }
+
+            public void SaveChanges()
+            {
+                KeepObjectivesModel.ObjectivesChecked = LocalObjectivesModel.ObjectivesChecked;
+                KeepObjectivesModel.ParametersChecked = LocalObjectivesModel.ParametersChecked;
+            }
+        }
+    }
+}
